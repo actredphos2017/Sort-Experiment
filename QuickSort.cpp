c115b9f269e64f17da21987510d7771c59fabe06
ARP2017 <actredphos2017@163.com>
2022-05-25 17:13:03 +0800

    hw

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,47 +1,180 @@
 #include <iostream>
+#include <vector>
+#include <time.h>
 
 using namespace std;
 
-typedef char ElemType;
+typedef int CRecode;
 
-struct SqList{
-    int key;
+typedef int DataType;
+
+class SeqList;
+class SeqNode;
+
+class SeqNode{
+    public:
+        int key;
+        int times;
+        SeqList* father;
+    public:
+        SeqNode(int _key, SeqList* _father);
 };
 
-void QuickSort(SqList* list, int begin_loc, int end_loc){
-    if(begin_loc == end_loc) return;
+class SeqList{
+    public:
+        vector<SeqNode> nodes;
+    public:
+        void Output();
+        void Create();
+        void RandCreate(int n);
+};
 
-    cout << begin_loc << " " << end_loc << " :   ";
-    for(int i = 0; i < 10; i ++)
-        cout << " " << list[i].key;
-    cout << endl;
-    cin.get();
-    
-    int p[2] = {begin_loc + 1, end_loc},
-        empty_loc = begin_loc;
-    SqList temp = list[empty_loc];
-    int i = 1;
-    while(p[0] < p[1]){
-        while((i ? list[p[i]].key > temp.key : list[p[i]].key < temp.key) && p[0] != p[1])
-            p[i] -= i*2-1;
-        list[empty_loc] = list[p[i]];
-        empty_loc = p[i];
-        i = (++i)%2;
+void SeqList::Output(){
+    for(auto i : this->nodes){
+        cout << i.key;
+        for(int j = 0; j < i.times; j ++)
+            cout << "*";
+        cout << " ";
     }
-    list[p[0]] = temp;
-    QuickSort(list, begin_loc, p[0]-1);
-    QuickSort(list, p[0]+1, end_loc);
 }
+
+void SeqList::Create(){
+    unsigned int n;
+    int key;
+    cout << "Please enter the number of nodes: ";
+    cin >> n;
+    cout << "Please enter the keys: ";
+    while(n --){
+        cin >> key;
+        nodes.push_back(SeqNode(key, this));
+    }
+}
+ Ann
+void SeqList::RandCreate(int n){
+    while(n --)
+        this->nodes.push_back(SeqNode(rand()%100, this));
+}
+
+SeqNode::SeqNode(int _key, SeqList* _father){
+    this->key = _key;
+    this->father = _father;
+    this->times = -1;
+    int t = -1;
+    for(auto i : _father->nodes)
+        if(_key == i.key && i.times != -1 && i.times > t)
+            t = i.times;
+    this->times = t + 1;
+}
+
+void DebugQuickSort_main(SeqList &list, int begin_loc, int end_loc, unsigned int &Compare_times, unsigned int &Recursion_times, unsigned int &Swap_times){
+    if(begin_loc >= end_loc) return;
+    Recursion_times ++;
+    
+    cout << endl << "From " << begin_loc << " to " << end_loc << " : ";
+    list.Output();
+    cout << endl;
+    
+    int p[2] = {begin_loc + 1, end_loc}, empty_loc = begin_loc, i = 1;
+    SeqNode temp = list.nodes[empty_loc];
+    while(p[0] < p[1]){
+        while((i ? list.nodes[p[i]].key >= temp.key : list.nodes[p[i]].key <= temp.key) && p[0] < p[1]){
+            i ? p[i] -- : p[i] ++;
+            Compare_times ++;
+        }
+        Swap_times ++;
+        list.nodes[empty_loc] = list.nodes[p[i]];
+        empty_loc = p[i];
+        i = 1 - i;
+    }
+    list.nodes[empty_loc] = temp;
+    DebugQuickSort_main(list, begin_loc, empty_loc - 1, Compare_times, Recursion_times, Swap_times);
+    DebugQuickSort_main(list, empty_loc + 1, end_loc, Compare_times, Recursion_times, Swap_times);
+}
+
+void DebugQuickSort(SeqList &list, int begin_loc, int end_loc){
+    unsigned int SqNode_nums        = end_loc - begin_loc + 1,
+                 Compare_times      = 0,
+                 Recursion_times    = 0,
+                 Swap_times         = 0;
+    cout << "Before sort:       ";
+    for(auto i : list.nodes)
+        cout << i.key << " ";
+    cout << endl;
+    DebugQuickSort_main(list, begin_loc, end_loc, Compare_times, Recursion_times, Swap_times);
+    cout << "Compare times:     " << Compare_times << endl;
+    cout << "Recursion times:   " << Recursion_times << endl;
+    cout << "Swap times:        " << Swap_times << endl;
+    cout << "After sort:        ";
+    list.Output();
+    cout << endl;
+}
+
+int Patition(SeqList &list, int begin_loc, int end_loc){
+	SeqNode temp = list.nodes[begin_loc];
+	int pivotkey = temp.key;
+	while (begin_loc < end_loc){
+		while(begin_loc < end_loc && list.nodes[end_loc].key >= pivotkey)
+            end_loc --;
+		list.nodes[begin_loc] = list.nodes[end_loc];
+		while(begin_loc < end_loc && list.nodes[begin_loc].key <= pivotkey)
+            begin_loc ++; 
+		list.nodes[end_loc] = list.nodes[begin_loc];
+	}
+	list.nodes[begin_loc] = temp;
+	return begin_loc;
+}
+
+void QSort(SeqList &list, int begin_loc, int end_loc) { //对顺序表L中的子序列L.r[low...end_loc]做快速排序
+	if(begin_loc < end_loc){
+        int pivotloc = Patition(list, begin_loc, end_loc);
+		QSort(list, begin_loc, pivotloc - 1);
+		QSort(list, pivotloc + 1, end_loc);    
+	}
+}
+
+void Unusual_QuickSort(vector<SeqNode> &sl){
+    if(sl.size() > 1){
+        vector<vector<SeqNode>> parts;
+        parts.resize(3);
+        parts[1].push_back(sl[0]);
+        for(auto i = sl.begin() + 1; i != sl.end(); i ++)
+            parts[2*(int)(i->key > parts[1][0].key)].push_back(*i);
+        sl.clear();
+        for(auto i : parts){
+            Unusual_QuickSort(i);
+            for(auto _i : i)
+                sl.push_back(_i);
+        }
+    }
+}
+
+#if 0
 
 int main(){
-    int keys[] = {59, 88, 17, 82, 3, 56, 57, 69, 55, 61};
-    SqList l[10];
-    for(int i = 0; i < 10; i ++)
-        l[i].key = keys[i];
-    cout << "hw" << endl;
-    QuickSort(l, 0, 9);
-    cout << "hw" << endl;
-    for(SqList i : l)
-        cout << i.key << " ";
+    srand(time(NULL));
+    SeqList l;
+    l.RandCreate(10);
+    Unusual_QuickSort(l.nodes);
+    l.Output();
     return 0;
 }
+
+#elif 0
+
+int main(){
+    srand(time(NULL));
+    SeqList l;
+    l.RandCreate(10);
+    QSort(l, 0, 9);
+    l.Output();
+    return 0;
+}
+
+#elif 1
+int main(){
+    SeqList l;
+    l.Create();
+    DebugQuickSort(l, 0, l.nodes.size()-1);
+    return 0;
+}
+#endif
